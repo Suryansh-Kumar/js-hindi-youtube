c18f46c124036d6d5c3f9654b79a9828e8f03079
Suryansh Kumar <suryansh7250@gmail.com>
2024-03-15 19:30:30 +0530

    Code till video 4 - Let, Const, Var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553;
+let accountEmail = "myemail@email.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+
+
+// accountId = 2
+console.log(accountId);
+
+accountEmail = "suryansh@gmail.com";
+accountPassword = "359875385";
+accountCity = "Bengaluru";
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
