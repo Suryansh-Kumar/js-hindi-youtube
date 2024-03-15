04fb421f955f90fcabcd6ca7753efca8873bc51e
Suryansh Kumar <suryansh7250@gmail.com>
2024-03-15 19:42:17 +0530

    Video 5

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; // ? treat all JS as the newer version
+
+// ! alert(3 + 3); // Only works in browser
+console.log(3 + 3);
+console.log("Hitesh");
+
+let name = "hitesh";
+let age = 18;
+let isLoggedIn = true;
+
+console.log(typeof "hitesh");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
