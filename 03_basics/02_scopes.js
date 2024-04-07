5380f8378dce4b919065cc88fa7b34f4c9d66f0b
Suryansh Kumar <suryansh7250@gmail.com>
2024-04-07 10:41:30 +0530

    Scope Level and Mini Hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -8,5 +8,40 @@ if (true) {
 }
 
 console.log(a);
-// console.log(b);
+// ! console.log(b);
 // console.log(c);
+
+function one() {
+	const username = "hitesh";
+	function two() {
+		const website = "youtube";
+        console.log(username);
+	}
+    // ! console.log(website);
+    two()
+}
+
+one()
+
+if (true) {
+    const username = "hitesh";
+    if (username === "hitesh") {
+        const website = " youtube";
+        console.log(username + website);
+    }
+    // ! console.log(website);
+}
+// ! console.log(username);
+
+// ?????????????????????????? INTERESTING ??????????????????????????
+
+function addone(num) {
+    return num + 1
+}
+
+console.log(addone(5))
+
+const addTwo = function(num) {
+    return num + 2
+}
+console.log(addTwo(5))
